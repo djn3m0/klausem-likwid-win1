3a6f254053f955a9533f47907af22afc03b28cb8
unrz254 <unrz254@saruman>
2009-10-08 08:16:04 +0200

    Added fixed counter instructions retirired for PErfmon V2 processors. Added hidden Cluster monitoring groups.

--- src/PerfCtr/perfCtrMain.c
+++ src/PerfCtr/perfCtrMain.c
@@ -111,6 +111,14 @@ int main (int argc, char** argv)
                 {
                     group = CPI;
                 }
+                else if (!strcmp("CLUSTER",optarg)) 
+                {
+                    group = CLUSTER;
+                }
+                else if (!strcmp("CLUSTER_FLOPS",optarg)) 
+                {
+                    group = CLUSTER_FLOPS;
+                }
                 else
                 {
                     customEvent = (char*) malloc((strlen(optarg)+10)*sizeof(char));
