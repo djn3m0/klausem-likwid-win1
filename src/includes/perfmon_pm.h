7cb82034fc30dda8959ecfbb05db7b901bcb6df0
jan <jan@windhoek.rrze.uni-erlangen.de>
2009-11-03 14:05:31 +0100

    Add Pentium M events.

--- src/includes/perfmon_pm.h
+++ src/includes/perfmon_pm.h
@@ -5,7 +5,7 @@
 #include <registers.h>
 
 
-void 
+    void 
 perfmon_init_pm(PerfmonThread *thread)
 {
     uint64_t flags = 0x0ULL;
@@ -18,103 +18,111 @@ perfmon_init_pm(PerfmonThread *thread)
 
     msr_write(cpu_id, MSR_PERFEVTSEL0, 0x0ULL);
     msr_write(cpu_id, MSR_PERFEVTSEL1, 0x0ULL);
-    msr_write(cpu_id, MSR_PERF_FIXED_CTR_CTRL, 0x0ULL);
-    msr_write(cpu_id, MSR_PERF_GLOBAL_CTRL, 0x0ULL);
-    msr_write(cpu_id, MSR_PERF_GLOBAL_OVF_CTRL, 0x0ULL);
-    msr_write(cpu_id, MSR_PEBS_ENABLE, 0x0ULL);
-
-    /* always initialize fixed counters
-     * FIXED 0: Instructions retired
-     * FIXED 1: Clocks unhalted */
-    msr_write(cpu_id, MSR_PERF_FIXED_CTR_CTRL, 0x22ULL);
 
     /* Preinit of two PMC counters */
     flags |= (1<<16);  /* user mode flag */
     flags |= (1<<19);  /* pin control flag */
-    flags |= (1<<22);  /* enable flag */
+    //    flags |= (1<<22);  /* enable flag */
 
     msr_write(cpu_id, MSR_PERFEVTSEL0, flags);
     msr_write(cpu_id, MSR_PERFEVTSEL1, flags);
 }
 
-void
+    void
 perfmon_printGroups_pm (void)
 {
-
-
+    printf("Performance Groups: Pentium M\n\n");
+    printf("FLOPS_DP: Double Precision MFlops/s\n");
+    printf("FLOPS_SP: Single Precision MFlops/s\n");
+    printf("L2: L2 cache bandwidth in MBytes/s\n");
+    printf("BRANCH: Branch prediction miss rate\n");
+    printf("CPI: cycles per instruction\n\n");
 }
 
 
-void
+    void
 perfmon_startCountersThread_pm(int thread_id)
 {
     int i;
-    uint64_t flags, uflags;
+    uint64_t flags;
     int cpu_id = threadData[thread_id].cpu_id;
+
+    /* zero the counter registers */
+    msr_write(cpu_id, threadData[thread_id].counters[0].counter_reg , 0x0ULL);
+    msr_write(cpu_id, threadData[thread_id].counters[1].counter_reg , 0x0ULL);
+
+    /* on p6 only MSR_PERFEVTSEL0 has the enable bit
+     * it enables both counters as long MSR_PERFEVTSEL1 
+     * has a valid configuration */
+    flags = msr_read(cpu_id, MSR_PERFEVTSEL0);
+    flags |= (1<<22);  /* enable flag */
+
+    if (perfmon_verbose)
+    {
+        printf("perfmon_start_counters: Write Register 0x%X , Flags: 0x%llX \n",MSR_PERFEVTSEL0,flags);
+    }
+
+    msr_write(cpu_id, MSR_PERFEVTSEL0, flags);
 
 }
 
-void 
+    void 
 perfmon_stopCountersThread_pm(int thread_id)
 {
-    uint64_t flags;
-    uint64_t uncore_cycles;
     int i;
     int cpu_id = threadData[thread_id].cpu_id;
 
+    msr_write(cpu_id, MSR_PERFEVTSEL0, 0x0ULL);
+    msr_write(cpu_id, MSR_PERFEVTSEL1, 0x0ULL);
 
+
+    for (i=0;i<NUM_PMC;i++) 
+    {
+        if (threadData[thread_id].counters[i].init == TRUE) 
+        {
+            threadData[thread_id].pc[i] = msr_read(cpu_id, threadData[thread_id].counters[i].counter_reg);
+        }
+    }
+
+    /*
+    threadData[thread_id].cycles = RDTSC;
+    */
 }
 
 
 
 
-PerfmonGroup
+    PerfmonGroup
 perfmon_getGroupId_pm (char* groupStr)
 {
-	PerfmonGroup group;
+    PerfmonGroup group;
 
-	if (!strcmp("FLOPS_DP",groupStr)) 
-	{
-		group = FLOPS_DP;
-	}
-	else if (!strcmp("FLOPS_SP",groupStr)) 
-	{
-		group = FLOPS_SP;
-	}
-	else if (!strcmp("L1",groupStr)) 
-	{
-		group = L1;
-	}
-	else if (!strcmp("L2",groupStr)) 
-	{
-		group = L2;
-	}
-	else if (!strcmp("MEM",groupStr)) 
-	{
-		group = MEM;
-	}
-	else if (!strcmp("DATA",groupStr)) 
-	{
-		group = DATA;
-	}
-	else if (!strcmp("BRANCH",groupStr)) 
-	{
-		group = BRANCH;
-	}
-	else if (!strcmp("TLB",groupStr)) 
-	{
-		group = TLB;
-	}
-	else if (!strcmp("CPI",groupStr)) 
-	{
-		group = CPI;
-	}
-	else
-	{
-		return NOGROUP;
-	}
+    if (!strcmp("FLOPS_DP",groupStr)) 
+    {
+        group = FLOPS_DP;
+    }
+    else if (!strcmp("FLOPS_SP",groupStr)) 
+    {
+        group = FLOPS_SP;
+    }
+    else if (!strcmp("L2",groupStr)) 
+    {
+        group = L2;
+    }
+    else if (!strcmp("BRANCH",groupStr)) 
+    {
+        group = BRANCH;
+    }
+    else if (!strcmp("CPI",groupStr)) 
+    {
+        group = CPI;
+    }
+    else
+    {
+        return NOGROUP;
+    }
 
-	return group;
+    return group;
 }
 
 
@@ -126,51 +134,27 @@ void perfmon_setupGroupThread_pm(int thread_id, PerfmonGroup group)
     switch ( group ) 
     {
         case FLOPS_DP:
-            setupCounterThread(thread_id, PMC0, "SIMD_COMP_INST_RETIRED_PACKED_DOUBLE");
-            setupCounterThread(thread_id, PMC1, "SIMD_COMP_INST_RETIRED_SCALAR_DOUBLE");
+            setupCounterThread(thread_id, PMC0, "EMON_SSE_SSE2_COMP_INST_RETIRED_PACKED_DP");
+            setupCounterThread(thread_id, PMC1, "EMON_SSE_SSE2_COMP_INST_RETIRED_SCALAR_DP");
             break;
 
         case FLOPS_SP:
-            setupCounterThread(thread_id, PMC0, "SIMD_COMP_INST_RETIRED_PACKED_SINGLE");
-            setupCounterThread(thread_id, PMC1, "SIMD_COMP_INST_RETIRED_SCALAR_SINGLE");
-            break;
-
-        case L1:
-            setupCounterThread(thread_id, PMC0, "SIMD_COMP_INST_RETIRED_PACKED_SINGLE");
-            setupCounterThread(thread_id, PMC1, "SIMD_INST_RETIRED_PACKED_SINGLE");
+            setupCounterThread(thread_id, PMC0, "EMON_SSE_SSE2_COMP_INST_RETIRED_ALL_SP");
+            setupCounterThread(thread_id, PMC1, "EMON_SSE_SSE2_COMP_INST_RETIRED_SCALAR_SP");
             break;
 
         case L2:
-            setupCounterThread(thread_id, PMC0, "L1D_REPL");
-            setupCounterThread(thread_id, PMC1, "L1D_M_EVICT");
-            break;
-
-        case MEM:
-            setupCounterThread(thread_id, PMC0, "BUS_TRANS_MEM_THIS_CORE_THIS_A");
-            break;
-
-        case DATA:
-            setupCounterThread(thread_id, PMC0, "INST_RETIRED_LOADS");
-            setupCounterThread(thread_id, PMC1, "INST_RETIRED_STORES");
+            setupCounterThread(thread_id, PMC0, "L2_LINES_IN_ALL_ALL");
+            setupCounterThread(thread_id, PMC1, "L2_LINES_OUT_ALL_ALL");
             break;
 
         case BRANCH:
-            setupCounterThread(thread_id, PMC0, "BR_INST_RETIRED_ANY");
-            setupCounterThread(thread_id, PMC1, "BR_INST_RETIRED_MISPRED");
+            setupCounterThread(thread_id, PMC0, "BR_INST_EXEC");
+            setupCounterThread(thread_id, PMC1, "BR_INST_MISSP_EXEC");
             break;
 
         case CPI:
-            setupCounterThread(thread_id, PMC0, "UOPS_RETIRED_ANY");
-            break;
-
-        case TLB:
-            setupCounterThread(thread_id, PMC0, "DTLB_MISSES_ANY");
-            setupCounterThread(thread_id, PMC1, "DTLB_MISSES_MISS_LD");
-            break;
-
-        case FRONTEND:
-            setupCounterThread(thread_id, PMC0, "UOPS_RETIRED_ANY");
-            setupCounterThread(thread_id, PMC1, "DTLB_MISSES_MISS_LD");
+            setupCounterThread(thread_id, PMC0, "UOPS_RETIRED");
             break;
 
         default:
