955fb51bca61df2afc0bd6a53ab667dd5feaabaf
unrz254 <unrz254@saruman>
2009-11-05 13:51:02 +0100

    Added support for Intel compiler

---
+++ include_ICC.mk
@@ -0,0 +1,16 @@
+CC  = icc
+DOXYGEN    = doxygen
+PERL       = perl
+
+ANSI_CFLAGS  = -strict-ansi
+ANSI_CFLAGS += -std=c99
+
+CFLAGS   = -g -O2 -Wno-format
+CPPFLAGS =
+LFLAGS   =
+DEFINES  = -D_GNU_SOURCE
+
+INCLUDES =
+LIBS     = 
+
+
